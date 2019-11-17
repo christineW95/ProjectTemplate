241e4cd8b0ae04a7c8185e51f25dde73f4bd1e77
Amir Hesham <amir.heham@vodafone.com>
2019-11-17 16:53:28 +0200

    Adding fonts for tha app

---
+++ ProjectTemplate/navigation/AppNavigation.js
@@ -0,0 +1,8 @@
+
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+
+// const AppNavigator = createStackNavigator({
+//   Screen: <View />
+// })
+// export default createAppContainer(AppNavigator)
