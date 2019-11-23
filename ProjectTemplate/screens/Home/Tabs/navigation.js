8590715e201105ae84097e5fdf4ae3a6712f02d7
christine <christinegeorge59@gmail.com>
2019-11-23 14:27:56 +0200

    fix navigation

--- ProjectTemplate/screens/Home/Tabs/navigation.js
+++ ProjectTemplate/screens/Home/Tabs/navigation.js
@@ -1,7 +1,7 @@
 import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
 import {createAppContainer} from 'react-navigation';
 import React, {Component} from 'react';
-import {Text, ImageBackground, Image, Dimensions,View} from 'react-native';
+import {Text, ImageBackground, Image, Dimensions, View} from 'react-native';
 var {width, height} = Dimensions.get('window');
 
 import Favorite from './Favorite';
@@ -14,35 +14,44 @@ import Cart from './Cart';
 // }
 // tabBarIcon : <Image source={require('./assets/images/react-native.png')}  style={styles.tabNavigatorProfileIcon} />
 const Tabs = createMaterialBottomTabNavigator(
-  {  
-    Home: { screen: Main,  
-        navigationOptions:{  
-            tabBarLabel:'Home',  
-            tabBarIcon: ({ tintColor }) => (  
-                <View>  
-                  <Image source={require('../../../assets/Images/Dashboard_Icon.png')} resizeMode='contain' style={{flex:1}}/>
-                    {/* <Icon style={[{color: tintColor}]} size={23} name={'home'}/>   */}
-                </View>),  
-        }  
-    },  
-    Cart: {  
-        screen: Cart,  
-        navigationOptions:{  
-            tabBarLabel:'Cart',  
-            tabBarIcon: ({ tintColor }) => (  
-                <View>  
-                <Image source={require('../../../assets/Images/Order_icon.png')} resizeMode='contain' style={{flex:1}}/>
-                </View>),  
-        }  
-    },  
-},  
-{  
-  initialRouteName: "Home",  
-  activeColor: '#f0edf6',  
-  inactiveColor: '#226557',  
-  barStyle: { backgroundColor: '#3BAD87' },  
-},  
- 
- 
-  );
+  {
+    Home: {
+      screen: Main,
+      navigationOptions: {
+        tabBarLabel: 'Home',
+        tabBarIcon: ({tintColor}) => (
+          <View>
+            <Image
+              source={require('../../../assets/Images/Dashboard_Icon.png')}
+              resizeMode="contain"
+              style={{flex: 1}}
+            />
+            {/* <Icon style={[{color: tintColor}]} size={23} name={'home'}/>   */}
+          </View>
+        ),
+      },
+    },
+    Cart: {
+      screen: Cart,
+      navigationOptions: {
+        tabBarLabel: 'Cart',
+        tabBarIcon: ({tintColor}) => (
+          <View>
+            <Image
+              source={require('../../../assets/Images/Order_icon.png')}
+              resizeMode="contain"
+              style={{flex: 1}}
+            />
+          </View>
+        ),
+      },
+    },
+  },
+  {
+    initialRouteName: 'Home',
+    activeColor: '#f0edf6',
+    inactiveColor: '#226557',
+    barStyle:{backgroundColor:'#F42C53',borderTopLeftRadius:10,borderTopRightRadius:10,overflow:'hidden'}
+  },
+);
 export default createAppContainer(Tabs);
--- ProjectTemplate/screens/Home/index.js
+++ ProjectTemplate/screens/Home/index.js
@@ -1,18 +1,22 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
-import {View,ImageBackground,Text,Dimensions} from 'react-native';
+import {View, ImageBackground, Text, Dimensions} from 'react-native';
 import HomeStyles from './Home.styles';
 import NavigationTabs from './Tabs';
+import {ScrollView} from 'react-native-gesture-handler';
+import Tabs from 'react-native-tabs';
 var {width, height} = Dimensions.get('window');
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {page: 'second'};
+  }
   render() {
     return (
-      <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 20 : 0, backgroundColor: 'green', }}>
-        <View style={{flex:1}}>
-            <NavigationTabs/>
-        </View>
-       </View>
+      <View style={{flex: 1}}>
+          <NavigationTabs/>
+      </View>
     );
   }
 }
