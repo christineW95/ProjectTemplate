78cbbecd8719b07942f1988629a4f76f3a0a8286
christine <christinegeorge59@gmail.com>
2019-11-29 00:16:47 +0200

    add firebase for package.json

--- ProjectTemplate/App.js
+++ ProjectTemplate/App.js
@@ -1,14 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react'
 import AppNavigation from './navigation/AppNavigation'
-
 const App = () => {
   return (
 
